b561cbbb5bca6b3a394401cb884484091b013970
Logan Ferris <FLogan800@gmail.com>
2025-04-18 15:38:50 -0500

    Add bubble sort

---
+++ src/bubble_sort.rs
@@ -0,0 +1,48 @@
+pub fn sort<T: PartialOrd>(data: &mut [T]) {
+    for i in 0..data.len() - 1 {
+        let mut swapped = false;
+
+        for j in 0..data.len() - i - 1 {
+            if data[j] > data[j + 1] {
+                data.swap(j, j + 1);
+                swapped = true;
+            }
+        }
+
+        if !swapped {
+            break;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_regular() {
+        let mut data = [3, 5, 2, 1, 4, 6, 9, 0, 8, 7];
+
+        sort(&mut data);
+
+        assert_eq!([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], data);
+    }
+
+    #[test]
+    fn test_reversed() {
+        let mut data = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+
+        sort(&mut data);
+
+        assert_eq!([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], data);
+    }
+
+    #[test]
+    fn test_duplicates() {
+        let mut data = [4, 2, 1, 0, 3, 3, 2, 4, 1, 0];
+
+        sort(&mut data);
+
+        assert_eq!([0, 0, 1, 1, 2, 2, 3, 3, 4, 4], data);
+    }
+}
